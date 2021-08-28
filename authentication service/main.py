db2588a57d45659252dde652d826542ae4da1d36
Shardul <khadyeshardul@gmail.com>
2021-08-29 02:21:05 +0530

    created user class

--- authentication service/main.py
+++ authentication service/main.py
@@ -1,9 +1,15 @@
+from src.db.models import init_db
 from typing import Optional
-
+from loguru import logger
 from fastapi import FastAPI
 from uvicorn import run
 
 app = FastAPI()
+
+@app.on_event('startup')
+async def init_process():
+    init_db()
+    logger.info('database initialized')
 
 
 if __name__ == '__main__':
---
+++ authentication service/src/db/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, Column, ForeignKey, Integer, String, Date, DateTime, Boolean, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+
+cdb = None
+DBSession: sessionmaker = None
+Base = declarative_base()
+
+def init_db():
+    global cdb , DBSession
+    config_path = f'config'
+    cdb = create_engine(f'sqlite:///{config_path}/config.db', connect_args={'check_same_thread': False})
+    Base.metadata.create_all(cdb)
+    Base.metadata.bind = cdb
+    DBSession = sessionmaker(bind=cdb)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer,primary_key=True)
+    username = Column(String)
+    password = Column(String)
